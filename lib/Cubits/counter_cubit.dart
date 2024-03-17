0ff97d406c4dd614f3c5d163f5570f32bf1f58fc
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-03-18 00:27:12 +0200

    Create points increment function and states

--- lib/Cubits/counter_cubit.dart
+++ lib/Cubits/counter_cubit.dart
@@ -3,4 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterBIncrementalState());
+  // In cubit it management function -> cubit -> update state (UI);
+  // Create Team Counter function
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+
+  void incrementPoints({required String teamName, required int buttonIndex}) {
+    if (teamName == 'A') {
+      teamAPoints += buttonIndex;
+      emit(CounterAIncrementalState());
+    } else {
+      teamBPoints += buttonIndex;
+      emit(CounterBIncrementalState());
+    }
+  }
 }
